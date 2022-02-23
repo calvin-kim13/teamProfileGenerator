68eef3719bbc543da3844345c8fab2afab0c335e
Calvin Kim <calvinkim1230@gmail.com>
2022-02-23 00:04:49 -0800

    added generateHTML js and added manager questions to index js

--- index.js
+++ index.js
@@ -5,11 +5,77 @@
 // back to x-roads again 
 // build team function
 
+// Packages to run the web application
 const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateHtml = require('./src/generateHTML');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const { number } = require('yargs');
 
+// Array to save the team members
+const employees = [];
 
+// Manager Questions
+async function managerQuestions() {
+    const {choice} = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the team manager's name?",
+            validate: (name) => {
+                if (name) {
+                    return true;
+                } else {
+                    console.log("Manager's name must be included.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the manager's id number?",
+            validate: (id) => {
+                if (isNaN(id)) {
+                    console.log(" *Manager's id number must be a number.");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the manager's email address?",
+            validate: (email) => {
+                if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+                    return true;
+                } else {
+                    console.log(" *Not a valid email address.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: "What is the manager's office number?",
+            validate: (officeNumber) => {
+                if (isNaN(officeNumber)) {
+                    console.log(' *Please enter a valid office number.');
+                    return false;
+                } else {
+                    return true;
+                }
+            }
 
-
+        }
+    ])
+}
 
 // Crossroads Function
 async function crossroads() {
@@ -21,7 +87,7 @@ async function crossroads() {
             choices: [
                 'Engineer',
                 'Intern',
-                'All-done'
+                'All-done! Build my team!'
             ]
         }
     ])
@@ -33,4 +99,4 @@ async function crossroads() {
         default: 
             return buildTeam();
     }
-};
+}
